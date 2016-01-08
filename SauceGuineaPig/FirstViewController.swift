386b463bd1d4072e12b7a3b8ac530f9db8dede92
Mehmet Gerceker <mehmetg@saucelabs.com>
2016-01-08 10:27:56 -0800

    Initial Commit

---
+++ SauceGuineaPig/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SauceGuineaPig
+//
+//  Created by Mehmet Gerceker on 1/8/16.
+//  Copyright © 2016 Awesome Company. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
