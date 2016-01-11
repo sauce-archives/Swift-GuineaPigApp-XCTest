999ef8fa83355c6844807097e21b21d3bb1b2b16
Mehmet Gerceker <mehmetg@saucelabs.com>
2016-01-11 15:19:52 -0800

    GiuineaPig is working both in webview and native mode. Tests neeed to be added.

--- SauceGuineaPig/FirstViewController.swift
+++ SauceGuineaPig/FirstViewController.swift
@@ -10,14 +10,53 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var textBox: UITextField!
+    @IBOutlet weak var uncheckedCheckBox: UISwitch!
+    @IBOutlet weak var checkedCheckBox: UISwitch!
+    @IBOutlet weak var yourComments: UILabel!
+    @IBOutlet weak var fbEmail: UITextField!
+    @IBOutlet weak var comments: UITextView!
+    @IBOutlet weak var clientTime: UILabel!
+    @IBOutlet weak var submit: UIButton!
+    @IBOutlet weak var iAmALink: UILabel!
+    @IBOutlet weak var userAgent: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let utgr = UITapGestureRecognizer.init(target: self, action: Selector("handleLinkTap:"))
+        iAmALink.addGestureRecognizer(utgr)
+        
+        clientTime.text = getTime()
+        let device = UIDevice()
+        userAgent.text = device.systemName + " " + device.systemVersion + " " + device.model
+        userAgent.text = userAgent.text! + " " + device.identifierForVendor!.UUIDString
+    }
+    func handleLinkTap(gestureRecogizer: UIGestureRecognizer){
+        let url = NSURL(string: "https://saucelabs.com/test-guinea-pig2.html")
+        let app = UIApplication.sharedApplication()
+        if(app.canOpenURL(url!)){
+            app.openURL(url!)
+        } else {
+            //ignore ;)
+        }
+    }
+    
+    func getTime() ->String{
+        return "\(NSDate().timeIntervalSince1970 * 1000)"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func handleSend(sender: AnyObject) {
+        yourComments.text = comments.text
+        comments.text = nil
+        checkedCheckBox.setOn(true, animated: true)
+        uncheckedCheckBox.setOn(false, animated: true)
+        fbEmail.text = nil
+        clientTime.text = getTime()
     }
 
 
--- SauceGuineaPig/SecondViewController.swift
+++ SauceGuineaPig/SecondViewController.swift
@@ -8,16 +8,35 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, UIWebViewDelegate {
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    @IBOutlet weak var backButton: UIButton!
+    
+    var url:NSURL = NSURL(string:"https://saucelabs.com/test/guinea-pig")!
+    
     override func viewDidLoad() {
+        webView.delegate = self;
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        backButton.hidden = true
+        let request = NSURLRequest(URL:url)
+        webView.userInteractionEnabled = true
+        webView.scalesPageToFit = true
+        webView.scrollView.scrollEnabled = true
+        webView.scrollView.showsHorizontalScrollIndicator = true
+        webView.loadRequest(request)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func webViewDidFinishLoad(m_webView: UIWebView) {
+        backButton.hidden = !m_webView.canGoBack
+        print(backButton.hidden)
     }
 
 
